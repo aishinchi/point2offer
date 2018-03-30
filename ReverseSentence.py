b660d32d316ae55c5bd8ffed2913a70c2bf203d8
aishinchi <dengchengyun54@qq.com>
2018-03-30 11:25:07 +0800

    100%(23ms)
    认识到了python的简洁.....

--- ReverseSentence.py
+++ ReverseSentence.py
@@ -1,25 +1,4 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def ReverseSentence(self, s):
-        # write code here
-        if s == "":
-            return ""
-        bigList = []
-        subList = ""
-        lens = len(s)
-        for i in range(lens):
-            if s[i] == " ":
-                bigList.append(subList)
-                subList = ""
-            else:
-                subList = subList + s[i]
-        bigList.append(subList)
-        bigList = bigList[::-1]
-        out = bigList[0]
-        for j in range(1,len(bigList)):
-            out += " "
-            out += bigList[j]
-        return out
-
-a = Solution()
-print(a.ReverseSentence("student. a am I"))
+        return " ".join(s.split(" ")[::-1])
